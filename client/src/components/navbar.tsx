0dec18606b5577cd09060b021e3950e95c2b3057
Manuj Rai <manuj.rai@fibre2fashion.com>
2025-02-13 17:27:59 +0530

    modified:   client/src/components/navbar.tsx

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -44,7 +44,7 @@ export default function Navbar() {
   );
 
   return (
-    <nav className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <nav className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 ml-4">
       <div className="container flex h-14 items-center">
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
