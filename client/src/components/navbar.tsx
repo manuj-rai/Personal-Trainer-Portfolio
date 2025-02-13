a21cb3357bb5f8559179d00afe94c36000ba0b2a
Manuj Rai <manuj.rai@fibre2fashion.com>
2025-02-13 17:55:01 +0530

    modified:   client/src/components/navbar.tsx 	modified:   client/src/index.css

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -44,10 +44,10 @@ export default function Navbar() {
   );
 
   return (
-    <nav className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 ml-4">
+    <nav className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center">
         <div className="mr-4 flex">
-          <Link href="/" className="mr-6 flex items-center space-x-2">
+          <Link href="/" className="mx-6 flex items-center space-x-2">
             <span className="font-bold">FitPro Trainer</span>
           </Link>
         </div>
